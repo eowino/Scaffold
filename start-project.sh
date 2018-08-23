8d944d4a1664cd70d095033829cc85741de33aef
Evans Owino <evansowino@hotmail.com>
2018-08-23 12:02:55 +0100

    project scaffold script

---
+++ start-project.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+    echo $'\n⚠️  Please provide a project name to scaffold!\n'
+    exit 111
+fi
+
+projectDir=~/Documents/Projects/
+cd $projectDir
+
+if [ -d "$1" ]; then
+    echo $'\n⚠️  The provided directory already exists: '$projectDir$1
+else
+    echo $'\n🛠️  Creating folder with the name: '$1
+    mkdir $1 && cd $1
+
+    echo $'\n🏁  Running "git init"\n'
+    git init
+
+    echo $'\n📕  Adding README.md file\n'
+    touch README.md && echo "# $1" > README.md
+    
+    echo $'\n📕  Project root: '$projectDir$1
+
+    echo ""
+    echo $'\n✏️  Launching Visual Studio Code\n'
+    code .
+
+    echo $'\n🎉   Happy coding!\n'
+fi
